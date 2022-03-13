b5a79cbdd0781b7bac2eb75e923b6e6321286c6b
BrunoMundim <brunomundimfranco@gmail.com>
2022-03-13 19:51:36 -0300

    Implementando rota por id de usuário

---
+++ src/routes/users.route.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction, Router } from 'express';
+
+//  get /users
+//  get /users/:uuid
+//  post /users
+//  put /users/:uuid
+//  delete /users/:uuid
+
+const usersRoute = Router();
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{ username: 'Bruno' }];
+    res.status(200).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+//  bancoDeDados.getUsersByUUid(uuid);
+    res.status(200).send({ uuid });
+});
+
+export default usersRoute;
