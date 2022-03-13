a6e2f17a5ec7b802e133ace21243173a0f314da1
BrunoMundim <brunomundimfranco@gmail.com>
2022-03-13 20:36:28 -0300

    Refatorando a rota de status

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-
-import express, {Request, Response, NextFunction} from 'express';
-import usersRoute from './routes/users.route';
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
@@ -9,13 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Configurações de Rotas
+app.use(statusRoute);
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso!'});
-});
 
 // Inicialização do servidor
 app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000!');
+  console.log("Aplicação executando na porta 3000!");
 });
