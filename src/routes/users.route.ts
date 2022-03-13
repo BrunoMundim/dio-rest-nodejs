fd8e9ded1104686fd1ee7166cd40787e590e06f0
BrunoMundim <brunomundimfranco@gmail.com>
2022-03-13 20:21:15 -0300

    Desenvolvendo e testando o método post

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -16,8 +16,12 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-//  bancoDeDados.getUsersByUUid(uuid);
     res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
 });
 
 export default usersRoute;
